6cee10de5932050b893cf3910b9350274b070cf7
sstankiewicz <sstankiewicz@gulfnews.com>
2019-06-20 15:14:25 +0400

    Create integration test.

--- src/test/java/com/sstankiewicz/testproject/service/GithubApiProxyImplTest.java
+++ src/test/java/com/sstankiewicz/testproject/service/GithubApiProxyImplTest.java
@@ -7,11 +7,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
