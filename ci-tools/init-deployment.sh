b1dcfa3add3191fda26bc320d9e75c74a146455e
sstankiewicz <sstankiewicz@gulfnews.com>
2019-06-22 16:43:46 +0400

    Fix deployment docker

--- ci-tools/init-deployment.sh
+++
@@ -1,3 +0,0 @@
-chmod 400 ci-tools/szymi.pem
-echo ls -l *.pem
-ssh -o StrictHostKeyChecking=no -i ci-tools/szymi.pem ec2-user@ec2-34-244-51-218.eu-west-1.compute.amazonaws.com < ci-tools/update-image.sh
