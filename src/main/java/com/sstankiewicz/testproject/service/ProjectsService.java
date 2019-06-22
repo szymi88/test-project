c36133861b91d7e4becf24b1cb58f3809e35be4d
sstankiewicz <sstankiewicz@gulfnews.com>
2019-06-22 20:05:53 +0400

    Create javadocs.

--- src/main/java/com/sstankiewicz/testproject/service/ProjectsService.java
+++ src/main/java/com/sstankiewicz/testproject/service/ProjectsService.java
@@ -4,5 +4,13 @@ import java.util.Optional;
 
 public interface ProjectsService {
 
+	/**
+	 * Returns a number of projects for the user
+	 *
+	 * @param user id
+	 * @return the number of users projects or an empty {@code Optional}
+	 * if the user was not found.
+	 *
+	 */
 	Optional<Integer> getProjectsCount(String user);
 }
--- src/main/java/com/sstankiewicz/testproject/service/ProjectsServiceImpl.java
+++ src/main/java/com/sstankiewicz/testproject/service/ProjectsServiceImpl.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+
+/**
+ * This class is responsible for the business logic related to the users repositories.
+ *
+ * @author sstankiewicz
+ */
 @Service
 public class ProjectsServiceImpl implements ProjectsService {
 
@@ -16,6 +22,15 @@ public class ProjectsServiceImpl implements ProjectsService {
 		this.restConsumer = restConsumer;
 	}
 
+
+	/**
+	 * Returns a number of projects for the user
+	 *
+	 * @param user id
+	 * @return the number of users projects or an empty {@code Optional}
+	 * if the user was not found.
+	 *
+	 */
 	public Optional<Integer> getProjectsCount(String user) {
 		return restConsumer.getUserRepositories(user).map(List::size);
 	}
